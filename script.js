65016bfeeb05c65475f5c0b8c77241d23b543c67
Murad Khan <murad.kh786@gmail.com>
2018-08-15 17:35:35 -0400

    Initial Files
    Initial files I started the project with, it is barebones and just pulls data directly from the Monster Hunter API and presents all of it at once.
    Goals:
    Add Vue.js to allow for users to choose what to see and keep it all in one page.
    Add more data from API and style it accordingly

---
+++ script.js
@@ -0,0 +1,98 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://mhw-db.com/weapons', true);
+
+request.onload = function () {
+    
+    const app = document.getElementById("root");
+        
+    const container = document.createElement("div");
+    container.setAttribute("class", "container");
+        
+    app.appendChild(container);
+    
+    
+    var data = JSON.parse(this.response);
+    
+    var newLine = "\r\n";
+    
+    data.forEach(weapon => {
+//        console.log(weapon.assets.image);
+//        console.log(weapon.name);   
+        
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
+        
+        const h1 = document.createElement('h1');
+        h1.textContent = weapon.name;
+        
+        const image = document.createElement("img");
+        image.src = weapon.assets.image;
+        
+        const p = document.createElement("p");
+        p.textContent = "Attack: " + weapon.attack.display + "\n" + "Raw: " + weapon.attack.raw;
+        
+        container.appendChild(card);
+        
+        card.appendChild(h1);
+        card.appendChild(image);
+        card.appendChild(p);
+        
+    })
+};
+
+//request.onload = function () {
+//    
+//    const app = document.getElementById("root");
+//    
+//    const logo = document.createElement("img");
+//    logo.src = "https://raw.githubusercontent.com/taniarascia/sandbox/master/ghibli/logo.png";
+//    
+//    const container = document.createElement("div");
+//    container.setAttribute('class', 'container');
+//    
+//    app.appendChild(logo);
+//    app.appendChild(container);
+//    
+//    // Begin accessing JSON data here
+//    var data = JSON.parse(this.response);
+//    
+//    if (request.status >= 200 && request.status < 400) {
+//        data.forEach(movie => {
+//        
+//            // Create a div with a card class
+//            const card = document.createElement('div');
+//            card.setAttribute('class', 'card');
+//            
+//            // Create an h2 and set the text content to the film's title
+//            const h1 = document.createElement('h1');
+//            h1.textContent = movie.title;
+//            
+//            // Create a p and set the text content to the film's description
+//            const p = document.createElement('p');
+//            movie.description = movie.description.substring(0, 300); // Limit to the first 300 characters
+//            p.textContent = `${movie.description}...`; // End with an ellipses
+//            
+//            // Append the cards to the container element
+//            container.appendChild(card);
+//            
+//            // Each card will contain an h1 and a p
+//            card.appendChild(h1);
+//            card.appendChild(p);
+//            
+//        
+//        
+//        });
+//    }
+//    else {
+//        const errorMessage = document.createElement('marquee');
+//        errorMessage.textContent = `It ain't workin!`
+//        app.appendChild(errorMessage);
+//    }
+//    
+//};
+
+// Send request
+request.send();
